b7a5bc6163cb8c464638ba365853abb927f32b09
Krondini <koro5940@colorado.edu>
2019-10-19 19:03:34 -0600

    Finished

--- Assignments/Assignment_PS7b/Rondini-Cat-PS7b-Q2.py
+++ Assignments/Assignment_PS7b/Rondini-Cat-PS7b-Q2.py
@@ -15,43 +15,49 @@ def bubbleSort(list_of_nums):
 	return num_flips
 
 def partition(A, left, right):
-	low = (left-1)
-	pivot = A[right]
+	low = (left-1) #Marker for lower boundary
+	pivot = A[right] #Pivot element
 
 	for i in range(left, right):
-		if A[i] < pivot:
-			low += 1
-			A[low], A[i] = A[i], A[low]
+		if A[i] < pivot: #Element belongs in lower boundary
+			low += 1 #Elongate lower bound
+			A[low], A[i] = A[i], A[low] #Swap
 
-	A[low+1], A[right] = A[right], A[i+1]
-	return (i+1)
+	A[low+1], A[right] = A[right], A[low+1] #Swap final elements
+	return (low+1) #Return the boundary marker
 
-def quickSort(list_of_nums, left, right):
-	if (left<right):
-		q = partition(list_of_nums, left, right)
-		quickSort(list_of_nums, left, q-1)
-		quickSort(list_of_nums, q+1, right)
+def quickSort(list_of_nums, left, right, num_flips):
+	if (left<right): #Check for base case
+		q = partition(list_of_nums, left, right) 
+		num_flips = quickSort(list_of_nums, left, q-1, num_flips) #Call on left side
+		num_flips = quickSort(list_of_nums, q+1, right, num_flips) #Call on right side
+
+	return num_flips+1 #Increment counter
+
 
 def main(argv):
 	# range_of_nums = int(input("Please enter how many numbers you would like to generate: "))
 	list_of_nums = []
 
-	for i in range(int(argv[1])): #Generate list of nums
-		list_of_nums.append(i+1)
+	for j in range(1, 12):
+		for i in range(2**j): #Generate list of nums
+			list_of_nums.append(i+1)
 
-	random.shuffle(list_of_nums) #Shuffle nums
-	
 
-	n_squared_flips = bubbleSort(list_of_nums)
-	print("There are %d flips in our list!" % (n_squared_flips))
-	print()
+		random.shuffle(list_of_nums) #Shuffle nums
+		
 
-	random.shuffle(list_of_nums)
-	print(list_of_nums)
-	print()
+		n_squared_flips = bubbleSort(list_of_nums)
+		print("There are %d flips in our list!" % (n_squared_flips))
 
-	quickSort(list_of_nums, 0, len(list_of_nums)-1)
-	print(list_of_nums)
+
+		random.shuffle(list_of_nums)
+
+
+		n_lgn_flips = quickSort(list_of_nums, 0, len(list_of_nums)-1, 0)
+		print("There are %d flips in our list!" % (n_lgn_flips))
+		print()
+
 
 if __name__ == '__main__':
 	main(sys.argv)
