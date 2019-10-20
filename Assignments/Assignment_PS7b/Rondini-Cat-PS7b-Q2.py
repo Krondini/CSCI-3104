8e8fbfb43d45b9e88a2288bcaa4d07f9fb854999
Krondini <koro5940@colorado.edu>
2019-10-19 20:50:09 -0600

    Added pictures of plots

--- Assignments/Assignment_PS7b/Rondini-Cat-PS7b-Q2.py
+++ Assignments/Assignment_PS7b/Rondini-Cat-PS7b-Q2.py
@@ -1,6 +1,7 @@
 import random
 import sys
-
+import matplotlib
+import matplotlib.pyplot as plt
 
 def bubbleSort(list_of_nums):
 	
@@ -38,18 +39,24 @@ def quickSort(list_of_nums, left, right, num_flips):
 def main(argv):
 	# range_of_nums = int(input("Please enter how many numbers you would like to generate: "))
 	list_of_nums = []
+	list_of_square_inputs = []
+	list_of_square_flips = []
+	list_of_lgn_inputs = []
+	list_of_lgn_flips = []
 
 	for j in range(1, 12):
 		for i in range(2**j): #Generate list of nums
 			list_of_nums.append(i+1)
 
+		list_of_square_inputs.append(i)
+		list_of_lgn_inputs.append(i)
 
 		random.shuffle(list_of_nums) #Shuffle nums
 		
 
 		n_squared_flips = bubbleSort(list_of_nums)
 		print("There are %d flips in our list!" % (n_squared_flips))
-
+		list_of_square_flips.append(n_squared_flips)
 
 		random.shuffle(list_of_nums)
 
@@ -57,6 +64,24 @@ def main(argv):
 		n_lgn_flips = quickSort(list_of_nums, 0, len(list_of_nums)-1, 0)
 		print("There are %d flips in our list!" % (n_lgn_flips))
 		print()
+		list_of_lgn_flips.append(n_lgn_flips)
+
+	fig1, n_lgn = plt.subplots()
+	fig2, n_squared = plt.subplots()
+
+	n_lgn.plot(list_of_lgn_inputs, list_of_lgn_flips)
+	n_lgn.set(xlabel="Size of inputs (2^n)", ylabel="Number of flips", title="Quick Sort")
+
+	n_squared.plot(list_of_square_inputs, list_of_square_flips)
+	n_squared.set(xlabel="Size of inputs (2^n)", ylabel="Number of flips", title="Bubble Sort")
+
+
+	fig1.savefig("BubbleSort.png")
+	fig2.savefig("QuickSort.png")
+
+	plt.show()
+
+
 
 
 if __name__ == '__main__':
