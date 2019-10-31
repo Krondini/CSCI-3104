533dfb7d3ea4039508e2d2589fef924e5fb83719
Krondini <koro5940@colorado.edu>
2019-10-31 12:51:43 -0600

    Working on Knapsack Problem

--- Assignments/Assignment_PS9a/Rondini-Cat-PS9a-Q1.py
+++ Assignments/Assignment_PS9a/Rondini-Cat-PS9a-Q1.py
@@ -1,35 +1,44 @@
-from random import randint
-from sys import argv
-from math import sqrt
 import numpy as np
-import csv
+import sys
+from random import randint
 
-def generateItems(num_items, max_weight):
-	
-	list_items = []
+'''
+Takes in two parameters
+	list_tuples = list of tuples in the form (item_value, item_weight)
+	max_weight = the maximum weight our knapsack can carry
 
-	for i in range(num_items):
-		list_items.append((randint(1, (max_weight)//2), randint(1, 100)))
+First check if an item's weight is greater than the max_weight
 
-	return list_items
+Returns filled in array of item values
+'''
+def knapsack(list_tuples, max_weight):
+	curr_element = list_tuples[0] #Current element is the first item
+	if curr_element[1] < max_weight: #We can fit item in our knapsack
+		return max(knapsack(list_tuples[1:], max_weight), curr_element[0] + knapsack(list_tuples[1:], max_weight- curr_element[1]))
 
-def main(args):
+	else: #Current item's weight is too large for knapsack
+		return knapsack(list_tuples[1:], max_weight)
 
-	num_items = int(args[1])
-	max_weight = int(args[2])
 
-	list_items = generateItems(num_items, max_weight)
-	array_of_items = np.array(list_items)
-	array_of_items.sort(axis=0)
-	print(array_of_items)
+def main(argv):
+	rows = int(input("Please enter the width of your array: "))
+	cols = int(input("Please enter the height of your array: ")) + 1
 
-	with open("Q1_Excel_Sheet.csv", 'w', newline='') as csvfile:
-		line = csv.writer(csvfile)
-		for i in range(len(array_of_items)):
-			line.writerows([array_of_items[i]])
-			# line.writerows(array_of_items[1])
+	global_arr = []
+	for row in range(rows):
+		curr_row = []
+		for col in range(cols):
+			curr_row.append(0)
+		global_arr.append(curr_row)
 
-	csvfile.close()
+	for row in global_arr:
+		print(row)
+
+
+
+	# max_val = knapsack(global_arr, cols)
+	# print(max_val)
+
 
 if __name__ == '__main__':
-	main(argv)
+	main(sys.argv)
