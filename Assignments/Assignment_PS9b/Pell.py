f00599b264b6ba082e5d0a2051ce97d8a29933c6
Krondini <koro5940@colorado.edu>
2019-11-07 13:49:45 -0700

    Added 'bottomUpPell' function

--- Assignments/Assignment_PS9b/Pell.py
+++ Assignments/Assignment_PS9b/Pell.py
@@ -6,7 +6,7 @@ M = []
 
 def Pell(n):
 	if(n == 0) or (n == 1):
-		return n
+		return 1
 
 	else:
 		return((2*Pell(n-1)) + Pell(n-2))
@@ -14,7 +14,7 @@ def Pell(n):
 
 def memoPell(n):
 	if(n == 0) or(n == 1):
-		return n
+		return 1
 
 
 	try:
@@ -24,6 +24,12 @@ def memoPell(n):
 	except IndexError:
 		return ((2*memoPell(n-1)) + memoPell(n-2))
 
+def bottomUpPell(n, P):
+	if(len(P) >= n):
+		return P[n-1]
+	else:
+		P.append(2*bottomUpPell(n-1) + bottomUpPell(n-2))
+		return P[-1]
 
 def main(args):
 	num_Pells = int(args[1])
@@ -32,13 +38,13 @@ def main(args):
 	for i in range(num_Pells + 1): #Generate Pell numbers
 		nth_Pell = memoPell(i)
 		M.append(nth_Pell)
-		# print("The %dth Pell number is: %d" % (i, nth_Pell))
+		print("The %dth Pell number is: %d" % (i, nth_Pell))
 
 
-	os.system("clear") #Clear screen
+	# os.system("clear") #Clear screen
 	
 
-	print("The %dth Pell number is: %d" % (num_Pells, M[-1]))
+	# print("The %dth Pell number is: %d" % (num_Pells, M[-1]))
 	end = time.time() #End timing
 	total = end - start #Calculate timing
 	print("Total time to calculate the first %d Pell numbers was: " % (num_Pells), end=" ")
