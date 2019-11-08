883e1129b66e675979b8209fdd51ae43b4d06f1b
Krondini <koro5940@colorado.edu>
2019-11-08 11:32:37 -0700

    Writing Pell function in C++

--- Assignments/Assignment_PS9b/Pell.py
+++ Assignments/Assignment_PS9b/Pell.py
@@ -2,7 +2,7 @@ from sys import argv
 import time
 import os
 
-M = []
+M = [1, 1]
 
 def Pell(n):
 	if(n == 0) or (n == 1):
@@ -25,20 +25,35 @@ def memoPell(n):
 		return ((2*memoPell(n-1)) + memoPell(n-2))
 
 def bottomUpPell(n, P):
-	if(len(P) >= n):
-		return P[n-1]
-	else:
-		P.append(2*bottomUpPell(n-1) + bottomUpPell(n-2))
-		return P[-1]
+	for i in range(2, n):
+		P.append(2*P[i-1] + P[i-2])
+
+	print(P)
+	return P[-1]
+
+def constantSpacePell(n):
+	Pell_0 = 1
+	Pell_1 = 1
+	next_Pell = 0
+	for i in range(2, n):
+		next_Pell = (2*Pell_0 + Pell_1)
+		Pell_0 = Pell_1
+		Pell_1 = next_Pell
+		print("The current Pell number is: %d" % next_Pell)
+
+	return next_Pell
 
 def main(args):
 	num_Pells = int(args[1])
 	start = time.time() #Begin timing
 
-	for i in range(num_Pells + 1): #Generate Pell numbers
-		nth_Pell = memoPell(i)
-		M.append(nth_Pell)
-		print("The %dth Pell number is: %d" % (i, nth_Pell))
+	# print(bottomUpPell(num_Pells, M))
+
+	print(constantSpacePell(num_Pells))
+	# for i in range(num_Pells + 1): #Generate Pell numbers
+	# 	nth_Pell = memoPell(i)
+	# 	M.append(nth_Pell)
+	# 	print("The %dth Pell number is: %d" % (i, nth_Pell))
 
 
 	# os.system("clear") #Clear screen
