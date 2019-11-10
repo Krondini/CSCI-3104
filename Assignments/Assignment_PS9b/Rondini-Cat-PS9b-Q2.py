fc44a47c449669fac29c193ebc83c483cbeccb68
Krondini <koro5940@colorado.edu>
2019-11-10 11:32:34 -0700

    Finished Constant Space implementation

--- Assignments/Assignment_PS9b/Rondini-Cat-PS9b-Q2.py
+++ Assignments/Assignment_PS9b/Rondini-Cat-PS9b-Q2.py
@@ -2,11 +2,21 @@ from random import randint
 
 def Q2bPartC(input_arr, n):
 	
-	if (n < 0):
-		return 0
+	best_solution_1 = 0 #Best solution if you take the n-1 best case
+	best_solution_2 = 0	#Best solution if you take the n-2 best case
 
-	else:
-		return max(Q2bPartC(input_arr, n-1), input_arr[n] + Q2bPartC(input_arr, n-2))
+	best_total_solution = 0
+
+	for i in range(0, n+1):
+		if (i%2 == 0): #Even cases
+			best_solution_1 += input_arr[i]
+
+		else: #Odd cases
+			best_solution_2 += input_arr[i]
+
+		best_total_solution = max(best_solution_1, best_solution_2)
+
+	return best_total_solution
 
 def Q2b(input_arr, DP_table, n):
 	
@@ -25,11 +35,13 @@ def main():
 	
 	input_arr = []
 	DP_table = []
-	while True:
+
+	while True: #Imput elements
 		new_entry = (input("Please enter a number, enter nothing to stop: "))
 		if (new_entry == ""): break
 
 		input_arr.append(int(new_entry))
+	
 	# num_credits = int(input("Please enter how many classes are possible: "))
 	# for i in range(num_credits):
 	# 	input_arr.append(randint(1, 10))
