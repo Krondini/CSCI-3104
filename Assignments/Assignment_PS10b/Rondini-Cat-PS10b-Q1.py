842a9c93dd34fd27700d19f102345ca5aedc642e
Krondini <koro5940@colorado.edu>
2019-11-14 11:41:19 -0700

    Started Assignment 10b

---
+++ Assignments/Assignment_PS10b/Rondini-Cat-PS10b-Q1.py
@@ -0,0 +1,20 @@
+from sys import argv
+
+def allignStrings(string_x, string_y, c_insert, c_delete, c_sub):
+	print("allignStrings")
+
+def extractAllignment(S_matrix, string_x, string_y, c_insert, c_delete, c_sub):
+	print("extractAllignment")
+
+def commonSubstrings(string_x, int_L, opt_edits):
+	print("commonSubstrings")
+
+
+def main(args):
+	print("main")
+	
+	for i in args:
+		print(i)
+
+if __name__ == '__main__':
+	main(argv)
